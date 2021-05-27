8c57b85a38e2e4c41c37ec937a425c2975a70133
tourtiere <leonard-dufour@hotmail.com>
2021-05-27 12:02:58 -0400

    add dynamic url

--- www/src/app/components/Editor/Editor.tsx
+++ www/src/app/components/Editor/Editor.tsx
@@ -8,18 +8,33 @@ import { useEffect, useRef, useState } from "preact/hooks";
 import { h, JSX } from "preact";
 import { ViewUpdate } from "@codemirror/view";
 
+function extractUrl() {
+    const url = new URL(window.location.href);
+    const paramRaw = url.searchParams.get("i");
+    if (!paramRaw) return "";
+    return decodeURIComponent(paramRaw);
+}
+
+function saveUrl(value: string) {
+    if (value === "") {
+        return window.history.pushState(value, "Fibomachine", ``);
+    }
+    window.history.pushState(value, "Fibomachine", `?i=${encodeURIComponent(value)}`);
+}
+
 interface Props {
     onChange: (value: string) => void;
 }
+//[your expresssion] ; [first term] , [second term] ...
 export function Editor({ onChange }: Props): JSX.Element {
     const codemirrorRef = useRef(null);
-    const [isloaded, setLoaded] = useState(false);
+    const initValue = extractUrl();
 
     useEffect(() => {
-        if (isloaded) return;
         document.title = `You clicked imes`;
         let editor = new EditorView({
             state: EditorState.create({
+                doc: initValue,
                 extensions: [
                     keymap.of([{ key: "Enter", run: () => null, preventDefault: true }]),
                     highlight,
@@ -29,6 +44,8 @@ export function Editor({ onChange }: Props): JSX.Element {
                     EditorView.updateListener.of((v: ViewUpdate) => {
                         if (!v.docChanged) return;
                         const text = v.state.doc.sliceString(0);
+                        saveUrl(text);
+
                         onChange(text);
                     }),
                 ],
@@ -36,8 +53,7 @@ export function Editor({ onChange }: Props): JSX.Element {
             parent: codemirrorRef.current,
         });
         editor.focus();
-        setLoaded(true);
-    });
+    }, []);
 
     return <div ref={codemirrorRef} />;
 }
--- www/src/app/router.tsx
+++ www/src/app/router.tsx
@@ -8,22 +8,21 @@ type TRoute = {
     content: () => JSX.Element;
 };
 
-const routes: Record<string, TRoute> = {
-    "": {
-        content: () => <Home />,
-    },
-    "/page": {
-        content: () => <Page />,
-    },
-    "/page/nested": {
-        content: () => <Nested />,
-    },
-};
-
 export function router(location: Location): JSX.Element {
     const path = location.pathname.replace(/\/$/, "");
 
-    const route: { content: () => JSX.Element } = routes[path];
+    const routes: Record<string, TRoute> = {
+        "": {
+            content: () => <Home />,
+        },
+        "/page": {
+            content: () => <Page />,
+        },
+        "/page/nested": {
+            content: () => <Nested />,
+        },
+    };
 
+    const route: { content: () => JSX.Element } = routes[path];
     return route ? route.content() : <NotFound />;
 }
